9b6ca28bd7f4e784f9a54297b4f1eb68b705bedd
carme <carme@LAPTOP-HHGLH0KG>
2021-10-01 16:15:53 +0200

    Añadir mensaje.

--- PruebaGitHub2/src/GitHubTest1.java
+++ PruebaGitHub2/src/GitHubTest1.java
@@ -1,8 +1,9 @@
 
 public class GitHubTest1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Esto es una prueba de GitHub");
 
 	}
 
